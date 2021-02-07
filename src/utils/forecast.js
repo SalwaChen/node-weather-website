33b5d3d6e6f221335b9dbc346dffea920dceed6d
Salwa Chen <salwa_h3@hotmail.com>
2021-02-07 23:06:49 +0200

    add more data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,11 +12,9 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
-      const str = ` Weather description:
-      ${body.current.weather_descriptions[0]} 
-        it is ${body.current.temperature}
-         and it feels like
-          ${body.current.feelslike}`;
+      const str = `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature}
+        degrees out. It feels like
+          ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}`;
       callback(undefined, str);
     }
   });
